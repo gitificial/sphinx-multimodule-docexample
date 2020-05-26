8702dd713f108aed1dcc84a9eab5dacf406ed658
gitificial <keyboardroaster@gmail.com>
2020-05-26 13:19:37 +0200

    Add files via upload

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. sphinx-multimodule-docexample documentation master file, created by
+   sphinx-quickstart on Tue May 26 12:47:24 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinx-multimodule-docexample's documentation!
+=========================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   module1
+   module2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ module1/classA.py
@@ -0,0 +1,26 @@
+class classA(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def func1(param1, param2):
+        """
+        Args:
+            param1: The first parameter. \n
+            param2: The second parameter.
+    
+        Returns:
+            The return value. True for success, False otherwise.
+        """
+        param1 = param2
+        return param1
+    
+    def func2():
+        """
+        Args:
+            Takes no parameters.
+    
+        Returns:
+            The return value is None.
+        """
+        return None
